922ef316a03daa68b20b13f452f62e669260389e
fullt1lt <daniiltupikin98@gmail.com>
2026-07-11 21:01:51 +0200

    lesson25

--- victoria/admin.py
+++ victoria/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category, Reviews
 # Register your models here.
 
 class CustomProduct(admin.ModelAdmin):
@@ -7,7 +7,10 @@ class CustomProduct(admin.ModelAdmin):
     search_fields = ("title",)
     list_filter = ("price",)
     fieldsets = (
-        ("Основная информация:", {"fields": ("title", "description", "image")}),
+        (
+            "Основная информация:",
+            {"fields": ("title", "description", "image", "category_id", "review_id")},
+        ),
         ("Бюрократия:", {"fields": ("price", "quantity", "isStock")}),
         ("Даты:", {"fields": ("create_at", "update_at")}),
     )
@@ -15,4 +18,16 @@ class CustomProduct(admin.ModelAdmin):
     readonly_fields = ("create_at", "update_at")
 
 
+class CustomCategory(admin.ModelAdmin):
+    list_display = ["title"]
+    search_fields = ("title",)
+
+
+class CustomReviews(admin.ModelAdmin):
+    list_display = ["authors", "rating", "is_published", "create_at"]
+    list_filter = ("create_at", "is_published")
+
+
 admin.site.register(Product, CustomProduct)
+admin.site.register(Category, CustomCategory)
+admin.site.register(Reviews, CustomReviews)
--- victoria/models.py
+++ victoria/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+class Reviews(models.Model):
+    text = models.TextField()
+    authors = models.CharField(max_length=20)
+    rating = models.PositiveIntegerField(default=5, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    is_published = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.text[:10]} - {self.rating}"
+
+
 class Product(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
@@ -8,6 +28,16 @@ class Product(models.Model):
     quantity = models.IntegerField()
     isStock = models.BooleanField(default=True)
     image = models.ImageField(blank=True, null=True, upload_to="products")
+    category_id = models.ForeignKey(
+        Category, on_delete=models.PROTECT, blank=True, null=True, related_name="category"
+    )
+    review_id = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name="review",
+        blank=True,
+        null=True,
+    )
     create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
